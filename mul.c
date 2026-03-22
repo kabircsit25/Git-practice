d82b305a7fd759f9146dea0439694692976a9eb9
kabircsit <kabircsit25@oic.edu.np>
2026-03-22 10:39:47 +0545

    Added 3 C files

---
+++ mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+ {
+    int a,b,mul;
+    printf("Enter a:");
+    scanf("%d", &a);
+    printf("Enter b:");
+    scanf("%d", &b);
+    mul=a*b;
+    printf("The multiply is: %d", mul);
+    return 0;
+ }
